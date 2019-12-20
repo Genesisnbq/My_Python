b0c50d14a800a1d1bda7a85a96e3412da8b17f25
gGenesisnbq <Genesis.nbq@gmail.com>
2019-12-20 10:33:43 +0800

    new python

--- Lesson06 函数基础/04_内置函数.py
+++ Lesson06 函数基础/04_内置函数.py
@@ -3,17 +3,17 @@
 li = dir(__build_class__)
 print(li)
 
-print('\n', '='*20)
+print('\n', '=' * 20)
 
 li = li[li.index('__ge__'):]  # ! index返回下标,
 print(li)
 
-print('\n','='*20)
+print('\n', '=' * 20)
 
 for i in range(len(li)):
-    print("#{}.{}()".format(i+1, li[i]))  # todo ()就是字符 (lll￢ω￢)
+    print("#{}.{}()".format(i + 1, li[i]))  # todo ()就是字符 (lll￢ω￢)
 
-print('\n', '='*20)
+print('\n', '=' * 20)
 
 li = [2, 5, 8]
 print(len(li))
@@ -41,19 +41,18 @@ print(dict(enumerate(li)))  # todo enumerate(li) 返回的是键值对
 b = '1+2+10'
 print(b)
 print(eval(b))
-print(eval('10')+10)  # * 转化为真正的数字
+print(eval('10') + 10)  # * 转化为真正的数字
 
-print('\n', '='*20)
+print('\n', '=' * 20)
 
 # ? exec() 识别赋值语句, 没有返回值
 b = 'x+y+z'
 x = 1
 y = 11
 z = 18
-print(eval(b)) #! 执行 字符串
+print(eval(b))  # ! 执行 字符串
 
-print('\n', '='*20, '\n')
-
+print('\n', '=' * 20, '\n')
 
 s = """
 z = 10
@@ -65,11 +64,11 @@ print('ok')
 exec(s)
 x = 10
 y = 15
-print('\n', '='*20, '\n')
+print('\n', '=' * 20, '\n')
 
 exec(s, {'x': 100, 'y': 999})  # ? 传参
 
-print('\n', '='*20, '\n')
+print('\n', '=' * 20, '\n')
 
 exec(s, {'x': 99, 'y': 1}, {'x': 1, 'y': 88})  # todo 逗号表达式以表达式右边为主
 
@@ -82,25 +81,25 @@ def test(x):
     return x > 10
 
 
-print('='*20)
+print('=' * 20)
 
 list2 = [10, 11, 2, 5, 6, 99]
 print(list(filter(test, list2)))
 
 # ? map 对数据进行处理
-print('='*20)
+print('=' * 20)
 
 
 def test2(x):
-    return x*10
+    return x * 10
 
 
 list3 = [1, 2, 3, 4, 5]
 print(list(map(test2, list3)))
-print('='*20)
-#* zip 将对象逐一的配对
+print('=' * 20)
+# * zip 将对象逐一的配对
 list4 = ['k1', 'k2', 'k3']
 list5 = ['v1', 'v2', 'v3']
 
-print(zip(list4,list5))
-print(list(zip(list4,list5)))
+print(zip(list4, list5))
+print(list(zip(list4, list5)))
--- Lesson07 函数作用域与匿名函数/02_作用域.py
+++ Lesson07 函数作用域与匿名函数/02_作用域.py
@@ -6,10 +6,64 @@
 
 num = 100
 
-
 def f():
-    print(num)
+    num = 200
+    print("1:", num)
 
+
+print("2:", num)
 
 f()
 
+print("3:",num)
+
+print('='*20)
+
+# ! global  在函数内部定义全局变量
+def  f1():
+    global num
+    num = 200
+    print("1:",num)
+
+f1()
+# * 已经被修改了
+print(num)
+
+print('='*20)
+
+# ! nonlocal  使用外层函数的变量
+def f2():
+    num = 66
+
+    def f3():
+        num = 88
+        num += 1
+        print("1:", num)
+    
+    f3()
+    print('2:',num)
+
+
+f2()  # ! 调用
+
+print('='*20)
+
+def f3():
+    num = 66
+
+    def f4():
+        nonlocal num
+        num += 1
+        print("1:", num)
+    
+    f4()
+    print('2:',num)
+
+f3()
+
+# * 总结:
+"""
+1.  函数内部定义的为局部变量, 其作用域是局部作用域, 函数外无法调用
+2.  函数外定义的为全局变量, 其作用域是全局作用域, 如果函数内想要进行修改, 需要 global
+3. 外层函数的变量, 如果想要在内层函数进行修改, 就需要用到 nonlocal
+"""
