ae6a67e9dcde017d3480b048046236be725e09ea
gGenesisnbq <Genesis.nbq@gmail.com>
2020-03-21 03:37:41 +0800

    new python

--- Attempt.py
+++ Attempt.py
@@ -1,10 +1,9 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+def calc(a):
+    if a == 0:
+        return 1
+    else:
+        return a*calc(a-1)
 
-nums = [5, 10, 11, 13, 15, 35, 50, 55, 72, 92, 204, 215]
-nums = [5, 10, 11, 13, 15, 35, 50, 55, 72, 92, 204, 215]
-plt.hist(nums, bins=10, range=None, density=False, weights=None,
-         cumulative=False, bottom=None, histtype='bar', align='mid',
-         orientation='vertical', rwidth=None, log=False, color=None,
-         label=None, stacked=False)
+
+n = int(input())
+print(calc(n))
